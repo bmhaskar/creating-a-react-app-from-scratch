348ce7c7d8f0195350e0561d8aa955803e4c2928
Bharat Mhaskar <bmhaskar@gmail.com>
2021-01-31 20:36:47 +0530

    Added build generation with code splitting

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
-import React, {FC} from "react";
+import React, {FC, lazy, Suspense} from "react";
 import { hot } from "react-hot-loader";
 import "./App.css";
 import image from './React-and-typescript.png';
-const App: FC = () => {  
-  
+import Regular from "./Regular";
+const LoadingComponent: FC = () =>  <div>Loading...</div>;
+const Lazy = lazy(()=> import('./Lazy'));
+const App: FC = () => {    
   return (
       <div className="App">
         <h1> Hello, World! with typescript </h1>
+        <h1> build integration </h1>
+        <Regular />
+        <Suspense fallback={<LoadingComponent />}>
+         <Lazy />
+        </Suspense>
         <img src={image} />
       </div>
     );  
---
+++ src/Regular.tsx
@@ -0,0 +1,5 @@
+import React, { FC } from "react";
+
+const Regular: FC = () => <h1> Regular </h1>;
+
+export default Regular; 
