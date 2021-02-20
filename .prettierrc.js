916a9ff0c4e37bacf3d11061ffee023ee1a515e9
Bharat Mhaskar <bmhaskar@gmail.com>
2021-02-20 15:02:17 -0800

    Fixed quotes

--- .prettierrc.js
+++ .prettierrc.js
@@ -5,4 +5,5 @@ module.exports = {
     printWidth: 120,
     tabWidth: 4,
     endOfLine: 'auto',
+    quoteProps: 'preserve',
 };
--- webpack.config.js
+++ webpack.config.js
@@ -8,61 +8,61 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const mode = process.env.NODE_ENV === 'production' ? 'production' : 'development';
 
 module.exports = {
-    entry: './src/index.tsx',
+    'entry': './src/index.tsx',
     mode,
-    module: {
-        rules: [
+    'module': {
+        'rules': [
             {
-                test: /\.(j|t)sx?$/,
-                exclude: /(node_modules|bower_components)/,
-                loader: 'babel-loader',
-                options: {
-                    cacheDirectory: true,
+                'test': /\.(j|t)sx?$/,
+                'exclude': /(node_modules|bower_components)/,
+                'loader': 'babel-loader',
+                'options': {
+                    'cacheDirectory': true,
                 },
             },
             {
-                test: /\.css$/,
-                use: ['style-loader', 'css-loader'],
+                'test': /\.css$/,
+                'use': ['style-loader', 'css-loader'],
             },
 
             {
-                test: /\.(png|jpg|gif)$/i,
-                dependency: { not: ['url'] },
-                type: 'asset/resource',
+                'test': /\.(png|jpg|gif)$/i,
+                'dependency': { 'not': ['url'] },
+                'type': 'asset/resource',
             },
         ],
     },
-    resolve: { extensions: ['*', '.js', '.jsx', '.ts', '.tsx'] },
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: '[name].[contenthash].js',
+    'resolve': { 'extensions': ['*', '.js', '.jsx', '.ts', '.tsx'] },
+    'output': {
+        'path': path.resolve(__dirname, 'dist'),
+        'filename': '[name].[contenthash].js',
     },
-    devtool: 'cheap-module-source-map',
-    devServer: {
-        contentBase: './dist',
-        port: 3000,
-        hotOnly: true,
-        open: true,
+    'devtool': 'cheap-module-source-map',
+    'devServer': {
+        'contentBase': './dist',
+        'port': 3000,
+        'hotOnly': true,
+        'open': true,
     },
-    optimization: {
-        moduleIds: 'deterministic',
-        runtimeChunk: 'single',
-        splitChunks: {
-            cacheGroups: {
-                vendor: {
-                    test: /[\\/]node_modules[\\/]/,
-                    name: 'vendors',
-                    chunks: 'all',
+    'optimization': {
+        'moduleIds': 'deterministic',
+        'runtimeChunk': 'single',
+        'splitChunks': {
+            'cacheGroups': {
+                'vendor': {
+                    'test': /[\\/]node_modules[\\/]/,
+                    'name': 'vendors',
+                    'chunks': 'all',
                 },
             },
         },
     },
-    plugins: [
+    'plugins': [
         new webpack.HotModuleReplacementPlugin(),
         new ForkTsCheckerWebpackPlugin(),
         new CleanWebpackPlugin(),
         new HtmlWebpackPlugin({
-            template: path.resolve(__dirname, 'public', 'index.html'),
+            'template': path.resolve(__dirname, 'public', 'index.html'),
         }),
     ],
 };
